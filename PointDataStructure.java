3d1a5d2850483edadb1a01f2e2833d8b96f6825c
AllaAbayed <ala@topxite.com>
2016-06-10 14:47:02 +0300

    First attempt

--- PointDataStructure.java
+++ PointDataStructure.java
@@ -4,22 +4,49 @@ public class PointDataStructure implements PDT {
 
 	int heapSize;
 
-	Point[] minHeap;
-	Point[] maxHeap;
-	int numOfElements;
-	//////////////// DON'T DELETE THIS CONSTRUCTOR ////////////////
+    //stores all the numbers less than the current median in a maxheap, i.e median is the maximum, at the root
+    private PriorityQueue<Point> maxheap;
+    //stores all the numbers greater than the current median in a minheap, i.e median is the minimum, at the root
+    private PriorityQueue<Point> minheap;
+
+    //comparators for PriorityQueue
+    private static final maxHeapComparator myMaxHeapComparator = new maxHeapComparator();
+    private static final minHeapComparator myMinHeapComparator = new minHeapComparator();
+
+    //////////////// DON'T DELETE THIS CONSTRUCTOR ////////////////
 	public PointDataStructure(Point[] points, Point initialYMedianPoint) {
+        maxheap = new PriorityQueue<Point>(11,myMaxHeapComparator);
+        minheap = new PriorityQueue<Point>(11,myMinHeapComparator);
 
-		MedianHeap medianHeap = new MedianHeap();
-		medianHeap.insert(initialYMedianPoint);
-
+        minheap.add(initialYMedianPoint);
+        for (int i = 0; i < points.length; i++) {
+            addPoint(points[i]);
+        }
 
 	}
 
+    /**
+     * Inserts into MedianHeap to update the median accordingly
+     * @param n
+     */
 	@Override
-	public void addPoint(Point point) {
-		// TODO Auto-generated method stub
-		
+	public void addPoint(Point n) {
+
+        // initialize if empty
+        if(isEmpty()){ minheap.add(n);}
+        else{
+            //add to the appropriate heap
+            // if n is less than or equal to current median, add to maxheap
+            if(Double.compare(n.getY(), median()) < 0){maxheap.add(n);}
+            // if n is greater than current median, add to min heap
+            else if (Double.compare(n.getY(), median()) > 0){minheap.add(n);}
+        }
+        heapSize++;
+
+        // fix the chaos, if any imbalance occurs in the heap sizes
+        //i.e, absolute difference of sizes is greater than one.
+        fixChaos();
+
 	}
 
 	@Override
@@ -58,7 +85,41 @@ public class PointDataStructure implements PDT {
 		return null;
 	}
 
-	//TODO: add members, methods, etc.
-	
+    /**
+     * Returns empty if no median i.e, no input
+     * @return
+     */
+    private boolean isEmpty(){
+        return maxheap.size() == 0 && minheap.size() == 0 ;
+    }
+    /**
+     * Re-balances the heap sizes
+     */
+    private void fixChaos(){
+        //if sizes of heaps differ by 2, then it's a chaos, since median must be the middle element
+        if( Math.abs( maxheap.size() - minheap.size()) > 1){
+            //check which one is the culprit and take action by kicking out the root from culprit into victim
+            if(maxheap.size() > minheap.size()){
+                minheap.add(maxheap.poll());
+            }
+            else{ maxheap.add(minheap.poll());}
+        }
+    }
+
+    /**
+     * returns the median of the numbers encountered so far
+     * @return
+     */
+    public double median(){
+        //if total size(no. of elements entered) is even, then median iss the average of the 2 middle elements
+        //i.e, average of the root's of the heaps.
+        if( maxheap.size() == minheap.size()) {
+            return ((double)maxheap.peek().getY() + (double)minheap.peek().getY())/2 ;
+        }
+        //else median is middle element, i.e, root of the heap with one element more
+        else if (maxheap.size() > minheap.size()){ return (double)maxheap.peek().getY();}
+        else{ return (double)minheap.peek().getY();}
+
+    }
 }
 
